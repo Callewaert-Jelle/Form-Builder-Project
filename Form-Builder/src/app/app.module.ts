b705689b3b63973a6e33518f6c45a051303abdc6
jelle-callewaert <61138716+jelle-callewaert@users.noreply.github.com>
2020-05-22 15:50:29 +0200

    Added FormBuilder module

--- Form-Builder/src/app/app.module.ts
+++ Form-Builder/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormBuilderModuleModule } from './form-builder-module/form-builder-module.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormBuilderModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
