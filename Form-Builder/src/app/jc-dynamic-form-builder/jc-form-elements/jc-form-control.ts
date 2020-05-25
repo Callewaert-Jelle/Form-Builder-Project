05550920be1c722ad66019fdd915cf08a2d47645
jelle-callewaert <61138716+jelle-callewaert@users.noreply.github.com>
2020-05-25 17:44:18 +0200

    extended form control

--- Form-Builder/src/app/jc-dynamic-form-builder/jc-form-elements/jc-form-control.ts
+++ Form-Builder/src/app/jc-dynamic-form-builder/jc-form-elements/jc-form-control.ts
@@ -1,5 +1,12 @@
 import { FormControl } from '@angular/forms'
 
-export class JcFormControl extends FormControl {
+export enum JcFormControlTypes {
+    text = 'Text',
+    number = 'Number'
+    // more
+}
 
+export class JcFormControl extends FormControl {
+    
+    type: JcFormControlTypes;
 }
--- Form-Builder/src/app/jc-dynamic-form-builder/jc-form-elements/jc-form-group.ts
+++ Form-Builder/src/app/jc-dynamic-form-builder/jc-form-elements/jc-form-group.ts
@@ -1,5 +1,11 @@
 import { FormGroup } from '@angular/forms'
+import { JcFormControl } from './jc-form-control'
 
-export class JcFormControl extends FormGroup {
+export class JcFormGroup extends FormGroup {
 
+    controls: {[key: string]: JcFormControl}
+
+    get (controlName: string): JcFormControl {
+        return <JcFormControl> super.get(controlName);
+    }
 }
